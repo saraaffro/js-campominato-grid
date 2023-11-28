341913c5abd6aa471e39c37c73e85ed6da195b0f
saraaffro <saraaffro@gmail.com>
2023-11-28 16:25:46 +0100

    feat: bonus difficoltà if

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -6,34 +6,95 @@ Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 */
 
+/*
+Bonus
+Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
+- con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+- con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+- con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+*/
+
 // selezione del'elemento contenitore
 const gridElement = document.getElementById("grid");
 
 // selezione bottone play
 const playButton = document.getElementById("play");
 
-// creo ciclo per griglia
-for (let i = 1; i <= 100; i++){
+// selezione select option
+const gameLevels = document.getElementById("levels");
 
-    // comparsa griglia al click del playbutton
-    playButton.addEventListener("click",
 
-        function(){
-            const myElement = createMyelement("div", "square");
+// const gameLevels = gameLevelsSelect.value;
 
-            gridElement.append(myElement);
-            myElement.append(i);
+// creo ciclo per griglia facile
+if(gameLevels.value === "1"){
 
-            // cambio colore dei quadrati al click
-            myElement.addEventListener("click",
-                function(){
-                    myElement.classList.add("clicked");
-                    console.log(i);
-                }
-            );
-        }
-    );
+    for (let i = 1; i <= 100; i++){
+        // comparsa griglia al click del playbutton
+        playButton.addEventListener("click",
+    
+            function(){
+                const myElement = createMyelement("div", "square");
+    
+                gridElement.append(myElement);
+                myElement.append(i);
+    
+                // cambio colore dei quadrati al click
+                myElement.addEventListener("click",
+                    function(){
+                        myElement.classList.add("clicked");
+                        console.log(i);
+                    }
+                );
+            }
+        );
+    }
+
+}else if(gameLevels.value === "2"){
+    
+    for (let i = 1; i <= 81; i++){
+        // comparsa griglia al click del playbutton
+        playButton.addEventListener("click",
+    
+            function(){
+                const myElement = createMyelement("div", "square");
+    
+                gridElement.append(myElement);
+                myElement.append(i);
+    
+                // cambio colore dei quadrati al click
+                myElement.addEventListener("click",
+                    function(){
+                        myElement.classList.add("clicked");
+                        console.log(i);
+                    }
+                );
+            }
+        );
+    }
+}else{
+    for (let i = 1; i <= 49; i++){
+        // comparsa griglia al click del playbutton
+        playButton.addEventListener("click",
+    
+            function(){
+                const myElement = createMyelement("div", "square");
+    
+                gridElement.append(myElement);
+                myElement.append(i);
+    
+                // cambio colore dei quadrati al click
+                myElement.addEventListener("click",
+                    function(){
+                        myElement.classList.add("clicked");
+                        console.log(i);
+                    }
+                );
+            }
+        );
+    }
 }
+
 
 
 // FUNZIONI
